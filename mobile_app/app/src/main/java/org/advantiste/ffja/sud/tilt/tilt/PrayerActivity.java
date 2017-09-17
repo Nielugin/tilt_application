61a89cb8339945b8944950ebce962c301031a02d
Capgemini <jean-julien.boucheron@capgemini.com>
2017-09-17 21:35:32 +0200

    Adding Activities for each page

---
+++ mobile_app/app/src/main/java/org/advantiste/ffja/sud/tilt/tilt/PrayerActivity.java
@@ -0,0 +1,10 @@
+package org.advantiste.ffja.sud.tilt.tilt;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by JBOUCHER on 17/09/2017.
+ */
+
+public class PrayerActivity extends AppCompatActivity {
+}
