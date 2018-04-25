4e5e65f71130f93a8fc13e2c54373cd9cb36442e
Nielugin <jjboucheron@gmail.com>
2018-04-25 08:31:51 +0200

    code quality check
    reducing complexity

--- MyGDCApplication/app/src/main/java/org/advantiste/ffja/sud/gdc/mygdcapplication/subpages/reading/fragments/BookRow.java
+++ MyGDCApplication/app/src/main/java/org/advantiste/ffja/sud/gdc/mygdcapplication/subpages/reading/fragments/BookRow.java
@@ -26,18 +26,13 @@ import java.util.HashMap;
  */
 public class BookRow extends Fragment {
 
-    private ArrayAdapter<String> m_adapterForSpinner;
-    private ArrayAdapter<Integer> m_adapterForSpinnerBegin;
     private ArrayAdapter<Integer> m_adapterForSpinnerEnd;
     private Spinner bookSpinner;
     private Spinner bookBegin;
     private Spinner bookEnd;
-    private String[] mBookArray;
     private int maxChapter;
     private Context context;
     private InteractionInterface interactionInterface;
-
-    private ImageButton closeButton;
 
     public void setInteractionInterface ( InteractionInterface interactionInterface ) {
         this.interactionInterface = interactionInterface;
@@ -65,9 +60,7 @@ public class BookRow extends Fragment {
      */
     // TODO: Rename and change types and number of parameters
     public static BookRow newInstance ( ) {
-        BookRow fragment = new BookRow ( );
-
-        return fragment;
+        return new BookRow ( );
     }
 
 
@@ -90,70 +83,17 @@ public class BookRow extends Fragment {
         context=view.getContext ();
 
             /* ************** Gestion des Spinner */
-        bookSpinner = (Spinner )  view.findViewById( R.id.bookSpinner);
-        mBookArray = getResources().getStringArray(R.array.book_values);
+        bookSpinner = view.findViewById( R.id.bookSpinner);
+        String[] mBookArray = getResources().getStringArray(R.array.book_values);
         ////////////////////////////////////////////////////////////////
         //create an arrayAdapter an assign it to the spinner
-        m_adapterForSpinner = new ArrayAdapter<> (getActivity (), android.R.layout.simple_spinner_item, mBookArray);
+        ArrayAdapter<String> m_adapterForSpinner = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, mBookArray);
         m_adapterForSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         bookSpinner.setAdapter(m_adapterForSpinner);
         bookSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener () {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                String bookName = parentView.getItemAtPosition ( position ).toString ( );
-                System.out.println(bookName);
-                BibleBook book = BibleBook.fromString (bookName);
-                Log.w(AddHistoryActivity.class.getName(),
-                        "Selected book : "+book);
-
-                BibleBookChapterAssociation bbca = new BibleBookChapterAssociation();
-                maxChapter = bbca.getBibleIntegerEnumMap().get(book);
-
-                bookBegin =  view.findViewById(R.id.bookBegin);
-                bookEnd = view.findViewById(R.id.bookEnd);
-
-                m_adapterForSpinnerBegin = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item);
-                m_adapterForSpinnerBegin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                bookBegin.setAdapter(m_adapterForSpinnerBegin);
-                m_adapterForSpinnerEnd = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item);
-                m_adapterForSpinnerEnd.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                bookEnd.setAdapter(m_adapterForSpinnerEnd);
-
-                for (int chap=1; chap <= maxChapter; chap++){
-                    m_adapterForSpinnerBegin.add(chap);
-                    m_adapterForSpinnerEnd.add(chap);
-                }
-
-                bookEnd.setSelection(m_adapterForSpinnerEnd.getCount());
-
-                bookBegin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener () {
-                    @Override
-                    public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-
-                        int chapBeginSelected = Integer.parseInt(parentView.getItemAtPosition(position).toString());
-                        m_adapterForSpinnerEnd.clear();
-                        for (int chap=chapBeginSelected; chap <= maxChapter; chap++){
-                            m_adapterForSpinnerEnd.add(chap);
-                        }
-                        bookEnd.setSelection(m_adapterForSpinnerEnd.getCount());
-                    }
-
-                    @Override
-                    public void onNothingSelected(AdapterView<?> parentView) {
-                        // your code here
-                    }
-
-                });
-
-                closeButton = (ImageButton ) view.findViewById ( R.id.deleteReadingButton );
-
-                closeButton.setOnClickListener ( new View.OnClickListener ( ) {
-                    @Override
-                    public void onClick ( View v ) {
-                        interactionInterface.removeAction ();
-
-                    }
-                });
+                onBookSelected(parentView, position, view);
 
             }
 
@@ -164,6 +104,62 @@ public class BookRow extends Fragment {
         });
 
         return  view;
+    }
+
+    private void onBookSelected(AdapterView<?> parentView, int position, View view) {
+        String bookName = parentView.getItemAtPosition ( position ).toString ( );
+        BibleBook book = BibleBook.fromString (bookName);
+        Log.w(AddHistoryActivity.class.getName(),
+                "Selected book : "+book);
+
+        BibleBookChapterAssociation bbca = new BibleBookChapterAssociation();
+        maxChapter = bbca.getBibleIntegerEnumMap().get(book);
+
+        bookBegin =  view.findViewById(R.id.bookBegin);
+        bookEnd = view.findViewById(R.id.bookEnd);
+
+        ArrayAdapter<Integer> m_adapterForSpinnerBegin = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item);
+        m_adapterForSpinnerBegin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        bookBegin.setAdapter(m_adapterForSpinnerBegin);
+        m_adapterForSpinnerEnd = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item);
+        m_adapterForSpinnerEnd.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        bookEnd.setAdapter(m_adapterForSpinnerEnd);
+
+        for (int chap=1; chap <= maxChapter; chap++){
+            m_adapterForSpinnerBegin.add(chap);
+            m_adapterForSpinnerEnd.add(chap);
+        }
+
+        bookEnd.setSelection(m_adapterForSpinnerEnd.getCount());
+
+        bookBegin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener () {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+
+                int chapBeginSelected = Integer.parseInt(parentView.getItemAtPosition(position).toString());
+                m_adapterForSpinnerEnd.clear();
+                for (int chap=chapBeginSelected; chap <= maxChapter; chap++){
+                    m_adapterForSpinnerEnd.add(chap);
+                }
+                bookEnd.setSelection(m_adapterForSpinnerEnd.getCount());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+
+        ImageButton closeButton = view.findViewById(R.id.deleteReadingButton);
+
+        closeButton.setOnClickListener (new View.OnClickListener ( ) {
+            @Override
+            public void onClick ( View v ) {
+                interactionInterface.removeAction ();
+
+            }
+        });
     }
 
 
